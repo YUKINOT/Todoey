d39af4884bd8bb9b0a2f2230293abebf28124a27
YUKINOT <yukinotogashi@hotmail.co.jp>
2019-06-02 12:27:23 +0900

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by 冨樫由城乃 on 2019/06/02.
+//  Copyright © 2019 Yukino Togashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
